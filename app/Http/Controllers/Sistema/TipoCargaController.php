2e6cccb5c497e4447884653b02b0beb55d0f9b97
ErickxAguileraO <erickaguileraonate@gmail.com>
2023-01-31 17:39:42 -0300

    tipo de carga editar y crear funcional

--- app/Http/Controllers/Sistema/TipoCargaController.php
+++ app/Http/Controllers/Sistema/TipoCargaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Sistema;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\cuenta\CreateTipoCargaRequest;
+use App\Http\Requests\cuenta\UpdateTipoCargaRequest;
 use App\Models\TipoCarga;
 use Illuminate\Http\Request;
 
@@ -28,6 +29,25 @@ class TipoCargaController extends Controller
             $tipoCarga->tic_estado = $request->slc_estado_tipo_carga;
             $tipoCarga->save();
             return redirect()->route('tipo.carga.index')->with(['message' => 'Se creo un nuevo tipo de carga', 'type' => 'success']);
+        } catch (\Throwable $th) {
+            return redirect()->back()->with(['message' => 'error de ingreso', 'type' => 'error']);
+        }
+    }
+
+    public function edit($id)
+    {
+        $tipoCarga = TipoCarga::findOrFail($id);
+        return view('sistema.tiposCarga.editar', compact('tipoCarga'));
+    }
+
+    public function update(UpdateTipoCargaRequest $request, int $id)
+    {
+        try {
+            $tipoCarga = TipoCarga::findOrFail($id);
+            $tipoCarga->tic_nombre = ucwords(strtolower($request->nombre_tipo_carga));
+            $tipoCarga->tic_estado = $request->slc_estado_tipo_carga;
+            $tipoCarga->save();
+            return redirect()->route('punto.carga.index')->with(['message' => 'Se edito un tipo carga', 'type' => 'success']);
         } catch (\Throwable $th) {
             return redirect()->back()->with(['message' => 'error de ingreso', 'type' => 'error']);
         }
